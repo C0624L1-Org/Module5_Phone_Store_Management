ff5f0045b5396d6a99e9c4e5d1b31dcef3ca8f18
DuyTana273 <duytana273@gmail.com>
2025-03-13 23:58:05 +0700

    merge code a Đình Anh

--- md5_phone_store_management/src/main/java/com/example/md5_phone_store_management/service/implement/ProductService.java
+++ md5_phone_store_management/src/main/java/com/example/md5_phone_store_management/service/implement/ProductService.java
@@ -4,17 +4,18 @@ import com.example.md5_phone_store_management.model.Product;
 import com.example.md5_phone_store_management.repository.IProductRepository;
 import com.example.md5_phone_store_management.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService implements IProductService {
-
     @Autowired
-    private IProductRepository iProductRepository;
-
+    IProductRepository productRepository;
+    // Đình Anh
     @Override
     public void saveProduct(Product product) {
-        iProductRepository.saveProduct(product.getName(),
+        productRepository.saveProduct(product.getName(),
                 product.getPurchasePrice(),
                 product.getSellingPrice(),
                 product.getCPU(),
@@ -27,5 +28,14 @@ public class ProductService implements IProductService {
                 product.getStockQuantity(),
                 product.getQrCode(),
                 product.getSupplier().getSupplierID());
+    }
+    //Tuấn Anh
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+    @Override
+    public Page<Product> searchProductByNameAndSupplier_NameAndPurchasePrice(String name, String supplierName, double purchasePrice, Pageable pageable) {
+        return productRepository.searchProductByNameAndSupplier_NameAndPurchasePrice(name,supplierName,purchasePrice,pageable);
     }
 }
